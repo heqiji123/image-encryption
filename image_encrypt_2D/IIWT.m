5dcdbfc68511d8d1a1274a00cea19edda24e0251
heqiji <heqijiHQJ@163.com>
2023-06-12 19:48:24 +0800

    Delete files

--- image_encrypt_2D/IIWT.m
+++
@@ -1,8 +0,0 @@
-function [ P ] = IIWT(a,b,c,d)
-lshaarInt = liftwave('haar','int2int');
-els = {'p',[-0.125 0.125],0};
-lsnewInt = addlift(lshaarInt,els);
-a=double(a);b=double(b);c=double(c);d=double(d);
-    p= (ilwt2(a,b,c,d,lsnewInt));
-    P=double(p);
-end
